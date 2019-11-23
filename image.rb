21d2de0c5a3cd3deedd9a3d42edbb03690a3ceb1
Jeremy Bright <jeremybright@outlook.com>
2019-11-23 12:32:05 +0000

    Submission

--- image.rb
+++ image.rb
@@ -1,15 +1,46 @@
 class Image
 
-  def initialize(image)
-    @image = image
+  def initialize(array)
+    @photo = array
   end
 
+  def get_numbers
+    numbers = []
+    @photo.each_with_index do |row, row_number|
+      row.each_with_index do |item, col_number|
+      if item == 1
+      numbers << [row_number, col_number]
+    end
+  end 
+end
+ numbers
+end
+
+
+def blur!
+  numbers = get_numbers
+  @photo.each_with_index do |row, row_number|
+    row.each_with_index do |item, col_number|
+     numbers.each do |found_row_number, found_col_number|
+
+    if row_number == found_row_number && col_number == found_col_number
+          @photo[row_number -1][col_number] = 1 unless row_number == 0 
+          @photo[row_number +1][col_number] = 1 unless row_number >= 3 
+          @photo[row_number][col_number -1] = 1 unless col_number == 0 
+          @photo[row_number][col_number +1] = 1 unless col_number >= 3 
+    end
+   end
+ end
+end
+end
+
+
 def output_image
-        @image.each do |x|
+        @photo.each do | x |
             puts x.join
         end
+      end
     end
-  end
 
 image = Image.new([
   [0, 0, 0, 0],
@@ -18,4 +49,7 @@ image = Image.new([
   [0, 0, 0, 0]
 ])
 
+image.output_image
+image.blur!
+puts 
 image.output_image
